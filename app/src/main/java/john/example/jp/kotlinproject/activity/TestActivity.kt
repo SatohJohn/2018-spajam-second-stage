020600f00dae62c8e5951f7e8445605dfc60d07b
john <sato.keita@furyu.jp>
2018-07-07 21:36:02 +0900

    activityを追加した

--- app/src/main/java/john/example/jp/kotlinproject/activity/TestActivity.kt
+++ app/src/main/java/john/example/jp/kotlinproject/activity/TestActivity.kt
@@ -1,4 +1,16 @@
 package john.example.jp.kotlinproject.activity
 
-class TestActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import john.example.jp.kotlinproject.R
+
+class TestActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+        // Example of a call to a native method
+//        sample_text.text = stringFromJNI()
+    }
 }
